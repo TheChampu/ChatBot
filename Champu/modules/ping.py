d2bec11496ce301562240a57c01484b2158e82a4
Champu <117635421+TheChampu@users.noreply.github.com>
2024-11-06 20:02:07 +0530

    Update ping.py

--- Champu/modules/ping.py
+++ Champu/modules/ping.py
@@ -27,7 +27,7 @@ async def ping(_, message: Message):
 
     ms = (datetime.now() - start).microseconds / 1000
     await loda.edit_text(
-        text=f"нey вαву!!\n{ChampuBot.name} ιѕ alιve 🥀 αnd worĸιng ғιne wιтн a pιng oғ\n➥ `{ms}` ms\n\n<b>|| мαdє ωιтн ❣️ ву [ᴅᴇᴠ](https://t.me/{OWNER_USERNAME}) ||</b>",
+        text=f"нey вαву!!\n{ChampuBot.name} ιѕ alιve 🥀 αnd worĸιng ғιne wιтн a pιng oғ\n➥ `{ms}` ms\n\n<b>|| мαdє ωιтн ❣️ ву [ᴄʜᴧᴍᴘᴜ](https://t.me/{OWNER_USERNAME}) ||</b>",
         reply_markup=InlineKeyboardMarkup(PNG_BTN),
     )
     if message.chat.type == ChatType.PRIVATE:
